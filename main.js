32d123f1e8d16d5ee30fedd2d61cc5aa675bb775
samhub31 <oshosamuel303@gmail.com>
2023-08-15 09:52:33 +0100

    Add HTML, CSS, and JavaScript files

---
+++ main.js
@@ -0,0 +1,54 @@
+/*=============== SHOW MENU ===============*/
+
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+/*========= MENU SHOW ========*/
+/* validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*======== MENU HIDDEN ========*/
+/* validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
